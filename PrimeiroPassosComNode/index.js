5900d631fa87592b33d1ab232aec65839278ac34
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2022-08-19 20:00:44 -0300

    Primeiro projeto para conhecendo node

---
+++ PrimeiroPassosComNode/index.js
@@ -0,0 +1,15 @@
+
+import chalk from 'chalk'
+
+
+console.log(chalk.blue('isso e um teste'))
+
+
+const paragrafo = 'Texto retorna por uma função'
+
+
+function texto(string){
+    return string;
+}
+
+console.log(chalk.red(paragrafo))
